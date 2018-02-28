fcafdbf04856e51ca7d86fcc7be9c765e433de3c
kyorohiro <kyorohiro@gmail.com>
2018-03-01 05:11:49 +0900

    ..

--- lib/src/ncanvas.dart
+++ lib/src/ncanvas.dart
@@ -29,31 +29,6 @@ class TinyFlutterNCanvas extends core.Canvas {
     canvas.save();
   }
 
-  @override
-  void clipRect(core.Rect rect, {Matrix4 m:null}) {
-    flush();
-    if(m == null) {
-       m = getMatrix();
-    }
-    Vector3 v1 = new Vector3(rect.x, rect.y, 0.0);
-    Vector3 v2 = new Vector3(rect.x, rect.y + rect.h, 0.0);
-    Vector3 v3 = new Vector3(rect.x + rect.w, rect.y + rect.h, 0.0);
-    Vector3 v4 = new Vector3(rect.x + rect.w, rect.y, 0.0);
-    v1 = m * v1;
-    v2 = m * v2;
-    v3 = m * v3;
-    v4 = m * v4;
-    flu.Path path = new flu.Path();
-    path.moveTo(v1.x, v1.y);
-    path.lineTo(v2.x, v2.y);
-    path.lineTo(v3.x, v3.y);
-    path.lineTo(v4.x, v4.y);
-//    canvas.clipPath(path);
-    clipVertex(new Vertices.list(
-        <double> [v1.x, v1.y,v2.x, v2.y, v3.x, v3.y,v4.x, v4.y],
-        <int>[0,1,2,0,2,3]));
-  }
-
   void clipVertex(core.Vertices vertices) {
     List<double> positions = (vertices as Vertices)._positions;
     List<int> indices  = (vertices as Vertices)._indices;
